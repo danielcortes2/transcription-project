74b2f87fcd283496c2152883ee03556d6a92988d
Daniel Cortés <danielcortescasadas6@gmail.com>
2025-02-12 19:51:25 +0100

    He hecho algunos cambios porque antes daba problemas de compatibilidad

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from api import transcribe
- 
+from app.api.transcribe import router as transcribe_router  # Importamos el router
+
 app = FastAPI(title="API de Transcripción de Audio")
- 
-app.include_router(transcribe.router, prefix="/audio")
- 
+
+# Incluimos el router del módulo transcribe.py
+app.include_router(transcribe_router, prefix="/api", tags=["Transcripción"])
